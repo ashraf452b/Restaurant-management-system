15e95d259f7a5ebee8734c475c85e244c296221f
Ashraful <ashraf452b@gmail.com>
2025-11-11 02:21:44 +0600

    update

--- users.py
+++ users.py
@@ -14,67 +14,41 @@ class Employee(User):
         self.salary=salary
         self.age=age
 
-
 class Admin(User):
-
+    employees=[]
     def __init__(self,name,phone,address,email):
         super().__init__(name,phone,address,email)
-        self.myemployes=[]
-
-
-    def add_emp(self,restaurant,emp):
-        restaurant.add_emp(emp)
-
-
-    def __repr__(self):
-        return f"{self.name} {self.phone} {self.address} {self.email}"
+        
     
+    def add_emp(self,restaurant,employee):
+        restaurant.add_emp(employee)
 
-class Menu:
-    def __init(self):
-        self.items=[]
-        self.menu=Fooditem()
-
-
-    def add_item(self,name):
-        self.items.append(name)
-
-    def find_item(self,name):
-        for item in self.items:
-            if item.lower()==name.lower():
-                return item
-        return None
-    
-    def remove_item(self,name):
-        item=self.find_item(name)
-        if item != None:
-            self.items.remove(name)
-        else:
-            print(f'This item not found')
-    
-    def show_menu(self):
-        print(f"name\tprice\tquantity")
-        for i in self.items:
-            print(f'{i.name}\t{i.quantity}\t{i.price}')
-            
-
+    def view_emp(self,restaurant):
+        restaurant.view_emp()
+        
+        
 class Restaurant:
+    employees=[]
     def __init__(self,name):
         self.name=name
-        self.employees=[]
-
-    def add_emp(self,emp):
-        self.employees.append(emp)
-        print(f"{emp.name} Added!!")
-
+    
+    def add_emp(self,employee):
+        self.employees.append(employee)
+        print(f"{employee.name} Added !!")
+    
     def view_emp(self):
-        for emp in self.myemployes:
-            print(emp.name,emp.phone,emp.address,emp.email,emp.designation,emp.salary,emp.age)
+        print('****Employees List****')
+        for emp in self.employees:
+            print(f"{emp.name} {emp.address} {emp.phone} {emp.email} {emp.designation} {emp.salary} {emp.age}")
 
 
-class Fooditem:
-    def __init__(self,name,quantity,price):
-        self.name=name
-        self.quantity=quantity
-        self.price=price
+res=Restaurant('Visca barca')
+    
+emp1=Employee('ashraf',123321,'dhaka','mail@hihii','chef',1200,18)
 
+ad=Admin('ashraf',1232321,'dhaka','mail@email')
+ad.add_emp(res,emp1)
+ad.view_emp(res)
+
+
+    
